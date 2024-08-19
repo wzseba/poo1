a8478cd7f54402ba4342f6459eafa2c7a4c6908b
wzseba <wzseba@gmail.com>
2024-08-18 22:55:49 -0300

    first commit

---
+++ src/main/Cliente.java
@@ -0,0 +1,14 @@
+package main;
+
+public class Cliente {
+
+	private String nombre;
+	
+	public Cliente(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public void solicitarEnvioFlores(String nomflore, String direccion) {
+		
+	}
+}
